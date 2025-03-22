680609c8a252688e7cace28b980f860c7b477378
Christopher K. Long <ckl45@cam.ac.uk>
2025-03-22 10:19:33 +0000

    Corrected version specific imports

--- src/saveable_objects/__init__.py
+++ src/saveable_objects/__init__.py
@@ -4,7 +4,7 @@ A python package for checkpointing, saving, and loading objects.
 
 import sys
 
-if sys.version_info[1] >= 8: # check if python is version 3.8 or later
+if sys.version_info[1] >= 10: # check if python is version 3.8 or later
     from ._saveable_object import SaveableObject
 else: # import from the python version 3.7 compatable file
     from ._saveable_object_3_7 import SaveableObject
--- src/saveable_objects/checkpointing/__init__.py
+++ src/saveable_objects/checkpointing/__init__.py
@@ -4,7 +4,7 @@ A collection of functions for checkpointing objects.
 
 import sys
 
-if sys.version_info[1] >= 8: # check if python is version 3.8 or later
+if sys.version_info[1] >= 10: # check if python is version 3.8 or later
     from ._checkpointing import failed, succeeded
 else: # import from the python version 3.7 compatable file
     from ._checkpointing_3_7 import failed, succeeded
